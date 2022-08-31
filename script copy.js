9ec3a2ee87f28fb788341d63bc4f2e7a02ad96b6
GerminaTech <98488330+germinatech@users.noreply.github.com>
2022-08-31 20:56:32 -0300

    novo

---
+++ script copy.js
@@ -0,0 +1,31 @@
+/******************************************/
+// SEM UTILIZAR O QUERY SELECTOR
+/******************************************/
+
+// selecione todos os cards
+
+
+// selecione todos os card do meio
+
+// selecione somente o card do meio da primeira secao
+
+
+// selecione somente os cards laterais da segunda secao
+
+/******************************************/
+
+/******************************************/
+// PODE UTILIZAR O QUERY SELECTOR
+/******************************************/
+
+// selecione a lista de vantagens do primeiro card
+
+// selecione todas as listas de vantagens
+
+
+// selecione todos os titulos dos cards secundarios da segunda secao
+
+
+// selecione as vantagens riscadas da primeira secao
+
+/******************************************/
